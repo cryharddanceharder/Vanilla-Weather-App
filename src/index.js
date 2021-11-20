234a57eed13928eead245760b64589b76c46b7e3
cryharddanceharder <beckyfoster5197@gmail.com>
2021-11-20 18:09:08 +0000

    added getForecast function to collect api info

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ function currentTemp(response) {
   weatherDescription.innerHTML = `${response.data.weather[0].description}`;
   let windSpeed = document.querySelector(".wind-speed");
   windSpeed.innerHTML = `wind: ${Math.round(response.data.wind.speed)}m/s`;
+  getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "25181b26ceb1bd83a6773b0a70ee242f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getPosition(position) {
@@ -61,7 +70,8 @@ function searchCity(city) {
   axios.get(apiUrl).then(currentTemp);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -82,8 +92,6 @@ function displayForecast() {
 }
 
 searchCity("Manchester");
-
-displayForecast();
 
 let buttonPress = document.querySelector("button");
 buttonPress.addEventListener("click", buttonClick);
